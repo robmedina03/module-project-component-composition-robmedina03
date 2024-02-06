fb5bf52c0b4de5a56a6d2a9c9c885cefbaadbc4d
Robert Medina <robertsmedina11@gmail.com>
2024-02-05 21:09:18 -0500

    module4 project

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,9 +1,27 @@
 import React from "react"
 import Figure from "./Figure"
+import styled from "styled-components"
+
+const StyledCard = styled.div`
+border: 1px solid black;
+border-radius: 12px;
+padding:2rem;
+
+h2{
+  font-size:2.5em;
+  margin  0 0 1rem 0;
+}
+
+p{
+  font-style: arial;
+  margin  0 0 1.5rem 0;
+}
+`
+
 
 export default function Card({title, text,imageUrl, date }){
     return (
-      <div className='card'>
+      <StyledCard className='card'>
       <h2>
         {title}
       </h2>
@@ -14,7 +32,7 @@ export default function Card({title, text,imageUrl, date }){
       caption = {date} />
       
   
-    </div>
+    </StyledCard>
   
     )
   }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,11 +1,21 @@
 import React from "react"
+import styled from "styled-components"
+
+const StyledFigure = styled.figure`
+max-width: 500px;
+
+figcaption{
+  font-style: italic
+}
+
+`
 
 export default 
 function Figure({imageUrl,caption}){
     return (
-      <figure>
+      <StyledFigure>
         <img src= {imageUrl} />
         <figcaption>photo taken on {caption}</figcaption>
-      </figure>
+      </StyledFigure>
     )
   }
